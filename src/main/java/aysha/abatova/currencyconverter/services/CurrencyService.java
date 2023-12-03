56d3cabee799d84daf1fe0b19a33fda7fdbc65a7
user 2 <abatova.Aisha@tbcacademy.edu.ge>
2023-12-03 11:50:36 +0500

    feat: request to external api and saving

--- src/main/java/aysha/abatova/currencyconverter/services/CurrencyService.java
+++ src/main/java/aysha/abatova/currencyconverter/services/CurrencyService.java
@@ -1,8 +1,19 @@
 package aysha.abatova.currencyconverter.services;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestTemplate;
+
+import aysha.abatova.currencyconverter.dtos.CurrencyRateExternalDto;
+import aysha.abatova.currencyconverter.entities.Currency;
 
 
 
@@ -20,14 +31,28 @@ public class CurrencyService {
     @Value("${apiURL}")
     private String apiURL;
     
+    @Autowired
+    private RestTemplate restTemplate;
 
    
     
 
 
     public void getRates() throws Exception {
-        throw new Exception("Not Implemented");
+        ResponseEntity<List<CurrencyRateExternalDto>> response = restTemplate.exchange(
+            apiURL,
+            HttpMethod.GET,
+            null,
+            new ParameterizedTypeReference<List<CurrencyRateExternalDto>>() {});
+        List<Currency> currencies = saveCurrencies(response.getBody());
+        populateAccountsIfNotPresent(currencies);
 
+    }
+
+    private List<Currency> saveCurrencies(List<CurrencyRateExternalDto> fullCurrencies) {
+
+        List<Currency> currencies = fullCurrencies.stream().map(ExternalCurrencyMapper::currencyMapper).collect(Collectors.toList());
+        return currencies.stream().map(this::upsertCurrency).toList();
     }
 
     
