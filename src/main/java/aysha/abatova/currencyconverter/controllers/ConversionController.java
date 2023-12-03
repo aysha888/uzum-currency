83bbb873895ec87870ef20f62541c4a0c82b2a4b
user 2 <abatova.Aisha@tbcacademy.edu.ge>
2023-12-03 19:58:18 +0500

    feat: route officialrates

--- src/main/java/aysha/abatova/currencyconverter/controllers/ConversionController.java
+++ src/main/java/aysha/abatova/currencyconverter/controllers/ConversionController.java
@@ -1,19 +1,26 @@
 package aysha.abatova.currencyconverter.controllers;
 
-
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
+import aysha.abatova.currencyconverter.dtos.CurrencyRateExternalDto;
+import aysha.abatova.currencyconverter.presenters.RatesPresenter;
+import aysha.abatova.currencyconverter.services.CurrencyService;
 
 @RestController
 public class ConversionController {
 
- 
+    @Autowired
+    private CurrencyService currencyService;
 
+    @GetMapping("/officialrates")
+    public RatesPresenter rates(@RequestParam(name = "date") String date,
+            @RequestParam(name = "currency", defaultValue = "USD") String charCode) {
+        CurrencyRateExternalDto rate = currencyService.getRate(date, charCode);
+        String rateResult = rate.getRate();
+        return new RatesPresenter(rateResult);
+    }
 
-
-     
-
-    
 }
---
+++ src/main/java/aysha/abatova/currencyconverter/presenters/RatesPresenter.java
@@ -0,0 +1,13 @@
+package aysha.abatova.currencyconverter.presenters;
+
+public class RatesPresenter {
+    private String rate;
+
+    public String getRate() {
+        return rate;
+    }
+
+    public RatesPresenter(String rate) {
+        this.rate = rate;
+    }
+}
