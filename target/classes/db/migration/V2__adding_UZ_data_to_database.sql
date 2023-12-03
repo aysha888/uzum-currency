75fb7331b75e89fe36fe2d2c7f048b4d60b652f9
user 2 <abatova.Aisha@tbcacademy.edu.ge>
2023-12-03 21:24:49 +0500

    chore: remove useless files

--- target/classes/db/migration/V2__adding_UZ_data_to_database.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO currencies (num_code, char_code, nominal, currency_name, rate, comission_from, comission_to, date) VALUES ('860', 'UZS', 1, 'Узбекский Сум', 1, 0, 0, '31.12.2023');
-INSERT INTO accounts (balance, currency_id) VALUES (1000000000000, (SELECT id FROM currencies WHERE char_code = 'UZS'));
-
